60a5eba3a6621c43fdd9fc50a4744606858153b6
Alin <alin.l.macau@gmail.com>
2020-03-28 13:59:12 +0200

    concurrency+io hommework

--- homework8_concurrency+io/src/Main.java
+++ homework8_concurrency+io/src/Main.java
@@ -1,2 +1,18 @@
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        Person p = new Person();
+        p.createPerson();
+
+        BufferedWriter out = new BufferedWriter(new FileWriter("out.txt"));
+
+        for (int i = 0; i < p.lista.size(); i++) {
+            Person person = p.lista.get(i);
+            if (person.s.equals(Sex.FEMALE)) {
+                out.write("Happy Birthday " + person.getName() + "(" + person.getBirthdate().getYear() + ")!" + "\n");
+
+            }
+        }
+    }
 }
